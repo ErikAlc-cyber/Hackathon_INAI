1e8e10d774d1a1b8c67f4d601f3e9c6102ff3395
Jose Uriel Rodriguez Ramirez <uri_055@hotmail.com>
2021-05-18 17:17:20 -0500

    'modify_database'

--- Codigo/database/index.sql
+++ Codigo/database/index.sql
@@ -3,14 +3,18 @@ USE DNE;
 
 CREATE TABLE IF NOT EXISTS Vacantes(
 	ID int PRIMARY KEY,
-	Sujeto_Obligado varchar(100),
-	Fecha_Modif date,
-	Periodo date,
-	Tot_plazas_BVacantes int,
-	Tot_plazas_CVacantes int,
-	Tot_plazas_Vacantes int,
-	Area varchar(50),
-	Nota varchar(50)
+	Sujeto_Obligado  varchar(100) NOT NULL,
+	Fecha_Modif  date NOT NULL,
+	Periodo  varchar(100) NOT NULL,
+	Tot_plazas_BVacantes  int NOT NULL,
+	Tot_plazas_CVacantes  int NOT NULL,
+	Tot_plazas_Vacantes   int NOT NULL,
+	Area   varchar(50) NOT NULL,
+	Nota   varchar(50) NULL
 );
 
+INSERT INTO Vacantes VALUES  (12365720, 'COMISION ESTATAL DE DERECHOS HUMANOS DEL ESTADO DE AGUASCALIENTES', '2021/04/25', 'Trimestral', 1, 1, 2,'Coordinación Administrativa','' );
+
 DESCRIBE Vacantes;
+
+
