6f3764bf2d2b74f92136392d04744cb63a6bbec2
Erik <Erik.senseya@gmail.com>
2021-05-19 16:08:51 -0500

    'frontend'

--- Codigo/views/DNE.php
+++ Codigo/views/DNE.php
@@ -22,23 +22,35 @@
   </ul>
   <div class="menu">
       <p>Filtros:</p>
-      <form action="DNE.php" method="post">
+      <form action="DNE.php" method="post" id="filtro">
         <label for="Estado">Estado:</label><br>
-        <input type="text" id="txtEst" value=""><br>
-        <label for="Area">Area:</label><br>
-        <input type="text" id="txtArea" value=""><br><br>
+        <script>
+          var x = document.createElement("SELECT");
+          x.setAttribute("id", "mySelect");
+          document.getElementById("filtro").appendChild(x);
+
+          var z = document.createElement("option");
+          z.setAttribute("value", "num1");
+          var t = document.createTextNode("Aguascalientes");
+          z.appendChild(t);
+          document.getElementById("mySelect").appendChild(z);
+
+        </script>
+        <br>
+        <label for="Area">Id:</label><br>
+        <input type="text" id="txtId" value=""><br><br>
         <button class="button">Buscar</button>
       </form>
   </div>
 
 <?php
 
-      echo '<table class="datos"> <tr> <th>Identificacion</th> <th>Nombre</th> <th>Ultima fecha disponible</th> <th>Actualiza la informacion</th> <th>Elimina este registro</th> </tr>';
+      echo '<table class="datos"> <tr> <th>Identificacion</th> <th>Nombre</th> <th>Ultima fecha disponible</th> <th>Mas informacion</th> <th>Elimina este registro</th> </tr>';
       foreach ($dato as $va ):
         //IMPORTANTE: Cuidado al modificar este enlace ya que envia el nombre del metodo y la id para mostrar al vacante a nuestro views.php   y asigne la vista correcta
         echo "<tr><td><a href='views.php?m=mostrar_uno&id=".$va['id_vacante']."'>".$va['id_vacante']."</a></td><td>".$va['Sujeto_Obligado']."</td><td>S./".$va['Fecha_Modif']."</td>";
 
-        echo "<td><a href='index.php?m=editar&id=".$va['id_vacante']."'>ACTUALIZAR</a></td> <td><a href='index.php?m=eliminar&id=".$va['id_vacante']."'>ELIMINAR</a></td>";
+        echo "<td><a href='informacion.php'>Mas Informacion</a></td> <td><a href='index.php?m=eliminar&id=".$va['id_vacante']."'>ELIMINAR</a></td>";
         echo "</tr>";
       endforeach;
       echo "</table>";
