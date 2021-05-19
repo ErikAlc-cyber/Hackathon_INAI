4a7d332d9bf55548423adaf72adbe716e1095e1c
danicelic <84361891+danicelic@users.noreply.github.com>
2021-05-19 17:39:12 -0500

    Add files via upload

--- Codigo/index.php
+++ Codigo/index.php
@@ -3,6 +3,18 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/index.css">
+    <style type="text/css">
+    @import url("avocado/stylesheet.css");
+.titulo {
+	font-family: avocado;
+	font-size: 50px;
+	font-weight: bold;
+}
+    body {
+	background-image: url(wallpapertip_teal-colour-wallpaper_389019.jpg);
+	text-align: center;
+}
+    </style>
     <title>DNE</title>
   </head>
   <body>
@@ -25,7 +37,9 @@
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       <p></p>
     </div>
-
-
+    <div>
+      
+      <embed src="AnyConv.com__KIZOA-Movie-Maker-1scuan20.mpg" width="32" height="32"></embed>
+    </div>
   </body>
 </html>
