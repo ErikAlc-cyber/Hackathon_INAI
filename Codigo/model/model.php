bf4b551b0278189afb7951b1676f017b6be2f0ac
Erik <Erik.senseya@gmail.com>
2021-05-20 12:39:46 -0500

    from mysql to postgrsql

--- Codigo/model/model.php
+++ Codigo/model/model.php
@@ -7,7 +7,7 @@ class Modelo{
     public function __construct(){
         $this->Modelo = array();
           //PORTFORWARDING AL SERVER REMOTO
-          $this->db=new PDO('mysql:host=app-278688fa-10dd-478a-9146-e68f61bc710f-do-user-8741944-0.b.db.ondigitalocean.com, 25060; dbname=dne', "dne","ohreisgk1e3hbuk9");
+          $this->db=new PDO('pgsql:host=app-278688fa-10dd-478a-9146-e68f61bc710f-do-user-8741944-0.b.db.ondigitalocean.com; port=25060; dbname=dne; user=dne; password=ohreisgk1e3hbuk9');
     }
 //MUESTRA DE INFORMACION
     public function mostrar($tabla,$condicion = "", $orden = "", $columna=""){
